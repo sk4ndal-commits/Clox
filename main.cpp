4b60c0154c34012fdc1e0621a4a1a7a928f4bb58
sascha-roggatz <sascharoggatz1999@outlook.de>
2024-12-04 23:41:18 +0100

    Refactor main.cpp and update HelloWorld.clox example.

--- main.cpp
+++ main.cpp
@@ -3,22 +3,27 @@
 #include <fstream>
 #include "Interpreter/Interpreter.h"
 
-int main(const int argc, char *argv[]) {
-
-    if (argc <= 1) {
+int main(const int argc, char *argv[])
+{
+    if (argc <= 1)
+    {
         std::cerr << "Error: No file provided" << std::endl;
         return 1;
     }
 
     const std::string fileName = argv[1];
-    if (!exists(std::filesystem::path(fileName))) {
-        std::cerr << "Error: The file: " << fileName << " does not exist" << std::endl;
+    if (!exists(std::filesystem::path(fileName)))
+    {
+        std::cerr << "Error: The file: " << fileName << " does not exist" <<
+                std::endl;
         return 1;
     }
 
     std::ifstream file(fileName);
-    if (!file.is_open()) {
-        std::cerr << "Error: The file " << fileName << " cannot be opened" << std::endl;
+    if (!file.is_open())
+    {
+        std::cerr << "Error: The file " << fileName << " cannot be opened" <<
+                std::endl;
         return 1;
     }
 
